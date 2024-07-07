24ae9a2e7eb1af71548256ee361d17a79d530037
Nikhil241103 <pekacoclover@gmail.com>
2024-07-07 20:26:48 +0530

    added media item

--- client/src/components/common/MediaSlide.jsx
+++ client/src/components/common/MediaSlide.jsx
@@ -3,6 +3,7 @@ import { SwiperSlide } from "swiper/react"
 import mediaApi from "../../api/modules/media.api"
 import AutoSwiper from "./AutoSwiper"
 import { toast } from "react-toastify"
+import MediaItem from "./MediaItem"
 
 const MediaSlide = ({ mediaType, mediaCategory }) => {
   const [medias, setMedias] = useState([])
@@ -26,7 +27,7 @@ const MediaSlide = ({ mediaType, mediaCategory }) => {
     <AutoSwiper>
       {medias.map((media, index) => (
         <SwiperSlide key={index}>
-          
+          <MediaItem media={media} mediaType={mediaType} />
         </SwiperSlide>
       ))}
     </AutoSwiper>
