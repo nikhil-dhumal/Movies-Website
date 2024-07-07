534959509198c93b2e1f409959e7ddb013248faa
Nikhil241103 <pekacoclover@gmail.com>
2024-07-07 20:22:32 +0530

    added auto swiper

--- client/src/components/common/MediaSlide.jsx
+++ client/src/components/common/MediaSlide.jsx
@@ -1,8 +1,35 @@
-import React from 'react'
+import { useEffect, useState } from "react"
+import { SwiperSlide } from "swiper/react"
+import mediaApi from "../../api/modules/media.api"
+import AutoSwiper from "./AutoSwiper"
+import { toast } from "react-toastify"
 
-const MediaSlide = () => {
+const MediaSlide = ({ mediaType, mediaCategory }) => {
+  const [medias, setMedias] = useState([])
+
+  useEffect(() => {
+    const getMedias = async () => {
+      const { response, err } = await mediaApi.getList({
+        mediaType,
+        mediaCategory,
+        page: 1
+      })
+
+      if (response) setMedias(response.results)
+      if (err) toast.error(err.message)
+    }
+
+    getMedias()
+  }, [mediaType, mediaCategory])
+
   return (
-    <div>MediaSlide</div>
+    <AutoSwiper>
+      {medias.map((media, index) => (
+        <SwiperSlide key={index}>
+          
+        </SwiperSlide>
+      ))}
+    </AutoSwiper>
   )
 }
 
