1ec9b351f1b49ec58065e1edeef95dbbaf0fa628
Nikhil241103 <pekacoclover@gmail.com>
2024-07-07 20:48:49 +0530

    corrected import of swiper module

--- client/src/components/common/NavigationSwiper.jsx
+++ client/src/components/common/NavigationSwiper.jsx
@@ -1,5 +1,5 @@
 import { Box } from "@mui/material"
-import { Navigation, Pagination } from "swiper"
+import { Navigation, Pagination } from "swiper/modules"
 import { Swiper } from "swiper/react"
 
 const NavigationSwiper = ({ children }) => {
