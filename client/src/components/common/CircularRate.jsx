76d9023a57bfebb2fcc85e86d4a4bbd957e3b217
Nikhil241103 <pekacoclover@gmail.com>
2024-07-07 22:35:42 +0530

    added rating logic

--- client/src/components/common/CircularRate.jsx
+++ client/src/components/common/CircularRate.jsx
@@ -1,4 +1,4 @@
-import { Box, Typography, CircularProgress } from "@mui/material";
+import { Box, Typography, CircularProgress } from "@mui/material"
 
 const CircularRate = ({ value }) => {
   return (
@@ -28,7 +28,7 @@ const CircularRate = ({ value }) => {
         </Typography>
       </Box>
     </Box>
-  );
-};
+  )
+}
 
-export default CircularRate;
+export default CircularRate
