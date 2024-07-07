1f0af0d3ea1cb8c24b5df3e665d8ed1e1cfcf3d7
Nikhil241103 <pekacoclover@gmail.com>
2024-07-07 20:21:24 +0530

    configured swiper

--- client/src/components/common/AutoSwiper.jsx
+++ client/src/components/common/AutoSwiper.jsx
@@ -1,8 +1,26 @@
-import React from 'react'
+import { Box } from "@mui/material"
+import { Swiper } from "swiper/react"
 
-const AutoSwiper = () => {
+const AutoSwiper = ({ children }) => {
   return (
-    <div>AutoSwiper</div>
+    <Box sx={{
+      "& .swiper-slide": {
+        width: {
+          xs: "50%",
+          sm: "35%",
+          md: "25%",
+          lg: "20.5%"
+        }
+      }
+    }}>
+      <Swiper
+        slidesPerView="auto"
+        grabCursor={true}
+        style={{ width: "100%", height: "max-content" }}
+      >
+        {children}
+      </Swiper>
+    </Box>
   )
 }
 
