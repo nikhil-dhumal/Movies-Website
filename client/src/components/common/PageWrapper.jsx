8338e8bc5b44ce3b7598458245b6b99c27c7569b
Nikhil241103 <pekacoclover@gmail.com>
2024-07-07 17:48:24 +0530

    added page wrapper component

---
+++ client/src/components/common/PageWrapper.jsx
@@ -0,0 +1,17 @@
+import { useEffect } from "react"
+import { useDispatch } from "react-redux"
+import { setAppState } from "../../redux/features/appStateSlice"
+
+const PageWrapper = ({ state, children }) => {
+  const dispatch = useDispatch()
+
+  useEffect(() => {
+    dispatch(setAppState(state))
+  }, [state, dispatch])
+
+  return (
+    children
+  )
+}
+
+export default PageWrapper
