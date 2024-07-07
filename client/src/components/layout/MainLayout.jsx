7c866b3f1c108e178dcbfdefedb1a797431036ed
Nikhil241103 <pekacoclover@gmail.com>
2024-07-07 17:54:15 +0530

    modified main layout

--- client/src/components/layout/MainLayout.jsx
+++ client/src/components/layout/MainLayout.jsx
@@ -1,8 +1,36 @@
-import React from 'react'
+import { Box } from "@mui/material"
+import { Outlet } from "react-router-dom"
 
 const MainLayout = () => {
+  
+
   return (
-    <div>MainLayout</div>
+    <>
+      {/* global loading */}
+      {/* global loading */}
+
+      {/* login modal */}
+      {/* login modal */}
+
+      <Box display="flex" minHeight="100vh">
+        {/* header */}
+        {/* header */}
+
+        {/* main */}
+        <Box
+          component="main"
+          flexGrow={1}
+          overflow="hidden"
+          minHeight="100vh"
+        >
+          <Outlet />
+        </Box>
+        {/* main */}
+      </Box>
+
+      {/* footer */}
+      {/* footer */}
+    </>
   )
 }
 
