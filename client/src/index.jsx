66e74cb1a99f954ce9faa57a1c0733ffdb6431b1
Nikhil241103 <pekacoclover@gmail.com>
2024-07-07 17:41:46 +0530

    added redux store

--- client/src/index.jsx
+++ client/src/index.jsx
@@ -1,7 +1,9 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import { Provider } from "react-redux"
 
 import App from './App'
+import store from './redux/store'
 
 import "@fontsource/roboto/300.css"
 import "@fontsource/roboto/400.css"
@@ -10,7 +12,9 @@ import "@fontsource/roboto/700.css"
 
 const root = ReactDOM.createRoot(document.getElementById('root'))
 root.render(
-  <React.StrictMode>
+  // <React.StrictMode>
+  <Provider store={store}>
     <App />
-  </React.StrictMode>
+  </Provider>
+  // </React.StrictMode>
 )
