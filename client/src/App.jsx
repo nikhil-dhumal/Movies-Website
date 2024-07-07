e642f3ba00be661ba1ed694f9eaf9ea4c779591b
Nikhil241103 <pekacoclover@gmail.com>
2024-07-07 17:58:52 +0530

    added routes

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,8 +1,61 @@
-import React from 'react'
+import { BrowserRouter, Route, Routes } from "react-router-dom"
+import { useSelector } from "react-redux"
+import { ThemeProvider } from "@mui/material/styles"
+import CssBaseline from "@mui/material/CssBaseline"
+import { ToastContainer } from "react-toastify"
+
+import MainLayout from "./components/layout/MainLayout"
+import PageWrapper from "./components/common/PageWrapper"
+import routes from "./routes/routes"
+import themeConfigs from "./configs/theme.configs"
 
 const App = () => {
+  const { themeMode } = useSelector((state) => state.themeMode)
+
   return (
-    <div>App</div>
+    <ThemeProvider theme={themeConfigs.custom({ mode: themeMode })}>
+      {/* config toastify */}
+      <ToastContainer
+        position="bottom-left"
+        autoClose={5000}
+        hideProgressBar={false}
+        newestOnTop={false}
+        closeOnClick
+        pauseOnFocusLoss
+        pauseOnHover
+        theme={themeMode}
+      />
+      {/* mui reset css */}
+      <CssBaseline />
+
+      {/* app routes */}
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<MainLayout />}>
+            {routes.map((route, index) => (
+              route.index ? (
+                <Route
+                  index
+                  key={index}
+                  element={route.state ? (
+                    <PageWrapper state={route.state}>{route.element}</PageWrapper>
+                  ) : route.element}
+                />
+              ) : (
+                <Route
+                  path={route.path}
+                  key={index}
+                  element={route.state ? (
+                    <PageWrapper state={route.state}>{route.element}</PageWrapper>
+                  ) : route.element}
+                />
+              )
+            ))}
+          </Route>
+        </Routes>
+      </BrowserRouter>
+      {/* app routes */}
+    </ThemeProvider>
   )
 }
 
