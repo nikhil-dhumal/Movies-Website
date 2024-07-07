1afe1c70c15322a2fb4d0ff1a4a2c06b701b8184
Nikhil241103 <pekacoclover@gmail.com>
2024-07-07 19:20:35 +0530

    added sidebar

--- client/src/components/common/Topbar.jsx
+++ client/src/components/common/Topbar.jsx
@@ -11,6 +11,7 @@ import { setAuthModalOpen } from "../../redux/features/authModalSlice"
 import { setThemeMode } from "../../redux/features/themeModeSlice"
 import Logo from "./Logo"
 import UserMenu from "./UserMenu"
+import Sidebar from "./Sidebar"
 
 const ScrollAppBar = ({ children, window }) => {
   const { themeMode } = useSelector((state) => state.themeMode)
@@ -42,8 +43,11 @@ const Topbar = () => {
     dispatch(setThemeMode(theme))
   }
 
+  const toggleSidebar = () => setSidebarOpen(!sidebarOpen)
+
   return (
     <>
+      <Sidebar open={sidebarOpen} toggleSidebar={toggleSidebar} />
       <ScrollAppBar>
         <AppBar elevation={0} sx={{ zIndex: 9999 }}>
           <Toolbar sx={{ alignItems: "center", justifyContent: "space-between" }}>
@@ -51,6 +55,7 @@ const Topbar = () => {
               <IconButton
                 color="inherit"
                 sx={{ mr: 2, display: { md: "none" } }}
+                onClick={toggleSidebar}
               >
                 <MenuIcon />
               </IconButton>
@@ -100,7 +105,6 @@ const Topbar = () => {
             </Stack>
             {user && <UserMenu />}
             {/* user menu */}
-
           </Toolbar>
         </AppBar>
       </ScrollAppBar>
