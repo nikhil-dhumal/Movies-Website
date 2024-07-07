e953afb1786e1aa0b5054d060c1a8c779bbb6259
Nikhil241103 <pekacoclover@gmail.com>
2024-07-07 17:25:32 +0530

    added roboto font

--- client/src/index.jsx
+++ client/src/index.jsx
@@ -3,7 +3,12 @@ import ReactDOM from 'react-dom/client'
 
 import App from './App'
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+import "@fontsource/roboto/300.css"
+import "@fontsource/roboto/400.css"
+import "@fontsource/roboto/500.css"
+import "@fontsource/roboto/700.css"
+
+const root = ReactDOM.createRoot(document.getElementById('root'))
 root.render(
   <React.StrictMode>
     <App />
