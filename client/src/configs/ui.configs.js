2b96b9ce6371b9f0064734fda917c4a198680a25
Nikhil241103 <pekacoclover@gmail.com>
2024-07-09 21:59:49 +0530

    modified gradient styles

--- client/src/configs/ui.configs.js
+++ client/src/configs/ui.configs.js
@@ -5,7 +5,7 @@ const uiConfigs = {
         backgroundImage: "linear-gradient(to top, rgba(0,0,0,1), rgba(0,0,0,0))"
       },
       light: {
-        backgroundImage: "linear-gradient(to top, rgba(245,245,245,0.3), rgba(0,0,0,0))"
+        backgroundImage: "linear-gradient(to top, rgba(245,245,245,1), rgba(0,0,0,0))"
       }
     },
     horizontalGradientBgImage: {
@@ -13,7 +13,7 @@ const uiConfigs = {
         backgroundImage: "linear-gradient(to right, rgba(0,0,0,1), rgba(0,0,0,0))"
       },
       light: {
-        backgroundImage: "linear-gradient(to right, rgba(245,245,245,0.3), rgba(0,0,0,0))"
+        backgroundImage: "linear-gradient(to right, rgba(245,245,245,0.5), rgba(0,0,0,0))"
       }
     },
     typoLines: (lines, textAlign) => ({
