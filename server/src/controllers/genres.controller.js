2dd27a11b3c8426fc8048e86a445ac17a108ce3a
Nikhil241103 <pekacoclover@gmail.com>
2024-07-09 22:02:12 +0530

    added genre controller

---
+++ server/src/controllers/genres.controller.js
@@ -0,0 +1,30 @@
+import responseHandler from "../handlers/response.handler.js"
+import tmdbApi from "../tmdb/tmdb.api.js"
+
+const getGenres = async (req, res) => {
+  try {
+    const movieGenres = await tmdbApi.mediaGenres({ mediaType: "movie" })
+    const tvGenres = await tmdbApi.mediaGenres({ mediaType: "tv" })
+
+    const response = { movie: movieGenres.genres, tv: tvGenres.genres }
+
+    return responseHandler.ok(res, response)
+  } catch {
+    responseHandler.error(res)
+  }
+}
+
+const getDiscoverMediaList = async (req, res) => {
+  try {
+    const { page, with_genres } = req.query
+    const { mediaType } = req.params
+
+    const response = await tmdbApi.discoverMediaList({ mediaType, with_genres, page })
+
+    return responseHandler.ok(res, response)
+  } catch {
+    responseHandler.error(res)
+  }
+}
+
+export default { getGenres, getDiscoverMediaList }
