410e9cf0f61256c35f6bd86ff5423836c8f22b3d
Nikhil241103 <pekacoclover@gmail.com>
2024-07-09 21:54:28 +0530

    added endpoint for fetching medias

--- client/src/api/modules/genre.api.js
+++ client/src/api/modules/genre.api.js
@@ -1,13 +1,21 @@
 import publicClient from "../client/public.client"
 
 const genreEndpoints = {
-  list: ({ mediaType }) => `${mediaType}/genres`
+  list: '/genres/list',
+  mediasList: ({ mediaType, genre, page }) => `/genres/${mediaType}?with_genres=${genre}&page=${page}`
 }
 
 const genreApi = {
-  getList: async ({ mediaType }) => {
+  getList: async () => {
     try {
-      const response = await publicClient.get(genreEndpoints.list({ mediaType }))
+      const response = await publicClient.get(genreEndpoints.list)
+
+      return { response }
+    } catch (err) { return { err } }
+  },
+  getMediasList: async ({ mediaType, genre, page }) => {
+    try {
+      const response = await publicClient.get(genreEndpoints.mediasList({ mediaType, genre, page }))
 
       return { response }
     } catch (err) { return { err } }
