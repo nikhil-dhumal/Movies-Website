7ec2d1ac47da6f7333e628f54ff986e483d2ae4f
Nikhil241103 <pekacoclover@gmail.com>
2024-07-09 22:08:32 +0530

    added comments

--- server/src/axios/axios.client.js
+++ server/src/axios/axios.client.js
@@ -1,13 +1,23 @@
 import axios from "axios"
 
+/**
+ * Sends a GET request to the specified URL and retrieves JSON data.
+ * @param {string} url - The URL to fetch data from.
+ * @returns {Promise<any>} - A promise resolving to the JSON data returned by the server.
+ */
 const get = async (url) => {
-  const response = await axios.get(url, {
-    headers: {
-      Accept: "application/json",
-      "Accept-Encoding": "identity"
-    }
-  })
-  return response.data
+  try {
+    const response = await axios.get(url, {
+      headers: {
+        Accept: "application/json",
+        "Accept-Encoding": "identity"
+      }
+    })
+    return response.data
+  } catch (error) {
+    console.error("Error fetching data:", error)
+    throw error // Propagate the error further if needed
+  }
 }
 
 export default { get }
