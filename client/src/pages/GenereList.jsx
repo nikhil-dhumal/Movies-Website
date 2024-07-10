2ff54b014a0237c41f50da526b65174519bb44e6
Nikhil241103 <pekacoclover@gmail.com>
2024-07-10 15:23:23 +0530

    added logic to sort medias

--- client/src/pages/GenereList.jsx
+++ client/src/pages/GenereList.jsx
@@ -1,35 +1,61 @@
-import { Box, Button, ButtonGroup, Grid, Stack, useScrollTrigger } from "@mui/material";
-import MediaItem from "../components/common/MediaItem";
-import { useState, cloneElement, useEffect } from "react";
-import { LoadingButton } from "@mui/lab";
-import uiConfigs from "../configs/ui.configs";
-import tmdbConfigs from "../api/configs/tmdb.configs";
-import usePrevious from "../hooks/usePrevious";
-import GenresSelect from "../components/common/GenresSelect";
-import { useDispatch, useSelector } from "react-redux";
-import { themeModes } from "../configs/theme.configs";
-import genreApi from "../api/modules/genre.api";
-import { setGlobalLoading } from "../redux/features/globalLoadingSlice";
-import { toast } from "react-toastify";
-import MediaGrid from "../components/common/MediaGrid";
+import { useState, useEffect } from "react"
+import { useDispatch, useSelector } from "react-redux"
+import { toast } from "react-toastify"
+
+import { LoadingButton } from "@mui/lab"
+import { Box, Button, ButtonGroup, Stack, Toolbar } from "@mui/material"
+
+import genreApi from "../api/modules/genre.api"
+
+import GenresSelect from "../components/common/GenresSelect"
+import MediaGrid from "../components/common/MediaGrid"
+import SortMethodSelect from "../components/common/SortMethodSelect"
+
+import uiConfigs from "../configs/ui.configs"
+import tmdbConfigs from "../api/configs/tmdb.configs"
+
+import { themeModes } from "../configs/theme.configs"
+
+import usePrevious from "../hooks/usePrevious"
+
+import { setGlobalLoading } from "../redux/features/globalLoadingSlice"
+
+const sortMethods = [
+  "original_title.asc",
+  "original_title.desc",
+  "popularity.asc",
+  "popularity.desc",
+  "revenue.asc",
+  "revenue.desc",
+  "primary_release_date.asc",
+  "oprimary_release_date.desc",
+  "title.asc",
+  "title.desc",
+  "vote_count.asc",
+  "vote_count.desc",
+]
 
 const GenereList = () => {
-  const { genres } = useSelector((state) => state.genres);
-  const [medias, setMedias] = useState([]);
-  const [mediaLoading, setMediaLoading] = useState(false);
-  const [currPage, setCurrPage] = useState(1);
-  const [mediaType, setMediaType] = useState(tmdbConfigs.mediaType.movie);
-  const [selectedGenre, setSelectedGenre] = useState(null);
-
-  const prevMediaType = usePrevious(mediaType);
-  const prevGenre = usePrevious(selectedGenre);
   const dispatch = useDispatch()
 
-  const onLoadMore = () => setCurrPage(currPage + 1);
+  const { genres } = useSelector((state) => state.genres)
+
+  const [currPage, setCurrPage] = useState(1)
+  const [medias, setMedias] = useState([])
+  const [mediaLoading, setMediaLoading] = useState(false)
+  const [mediaType, setMediaType] = useState(tmdbConfigs.mediaType.movie)
+  const [selectedGenre, setSelectedGenre] = useState(null)
+  const [sortMethod, setSortMethod] = useState(null)
+
+  const prevGenre = usePrevious(selectedGenre)
+  const prevMediaType = usePrevious(mediaType)
+  const prevSortMethod = usePrevious(sortMethod)
+
+  const onLoadMore = () => setCurrPage(currPage + 1)
 
   useEffect(() => {
     window.scrollTo(0, 0)
-  }, [mediaType, selectedGenre])
+  }, [mediaType, selectedGenre, sortMethod])
 
   useEffect(() => {
     const getMedias = async () => {
@@ -39,6 +65,7 @@ const GenereList = () => {
       const { response, err } = await genreApi.getMediasList({
         mediaType,
         genre: genres[mediaType][selectedGenre].id,
+        sort_by: sortMethods[sortMethod],
         page: currPage
       })
 
@@ -53,8 +80,9 @@ const GenereList = () => {
     }
 
     if (mediaType !== prevMediaType) {
-      setSelectedGenre()
+      setSelectedGenre(null)
       setMedias([])
+      setSortMethod(null)
       setCurrPage(1)
     }
 
@@ -63,33 +91,40 @@ const GenereList = () => {
       setCurrPage(1)
     }
 
+    if (sortMethod !== prevSortMethod) {
+      setMedias([])
+      setCurrPage(1)
+    }
+
     if (selectedGenre) getMedias()
   }, [
     mediaType,
     selectedGenre,
+    sortMethod,
     prevMediaType,
     currPage,
     dispatch
   ])
 
   return (
-    <Box sx={{ ...uiConfigs.style.mainContent }}>
+    <>
+      <Toolbar />
       <Box
         sx={{
           p: 2,
-          px: 12,
-          mt: { sm: "10% !important", sm: "8% !important", lg: "3.1% !important" },
+          px: { xs: 2, lg: 12 },
           position: "fixed",
           width: "100%",
-          left: "0",
           zIndex: 100,
           backgroundColor: "background.paper"
         }}
       >
-        <Stack direction="row" alignItems="center" justifyContent="space-between">
-          <GenresSelect mediaType={mediaType} selectedGenre={selectedGenre} setSelectedGenre={setSelectedGenre} />
-          <ButtonGroup>
+        <Stack alignItems="center" justifyContent="space-between" sx={{ flexDirection: { xs: "column", sm: "row" }, width: "100%" }}>
+          <ButtonGroup
+            sx={{ mb: { xs: 3, sm: 0 }, width: { xs: "100%", sm: "auto" } }}
+          >
             <Button
+              fullWidth
               size="large"
               variant={mediaType === tmdbConfigs.mediaType.movie ? "contained" : "text"}
               sx={{
@@ -100,6 +135,7 @@ const GenereList = () => {
               {tmdbConfigs.mediaType.movie}
             </Button>
             <Button
+              fullWidth
               size="large"
               variant={mediaType === tmdbConfigs.mediaType.tv ? "contained" : "text"}
               sx={{
@@ -110,28 +146,34 @@ const GenereList = () => {
               {tmdbConfigs.mediaType.tv}
             </Button>
           </ButtonGroup>
+          <Stack direction="row" alignItems="center" justifyContent="flex-end" spacing={2} sx={{ flexGrow: 1, width: { xs: "100%", sm: "auto" } }}>
+            <GenresSelect mediaType={mediaType} selectedGenre={selectedGenre} setSelectedGenre={setSelectedGenre} />
+            <SortMethodSelect sortMethod={sortMethod} setSortMethod={setSortMethod} />
+          </Stack>
         </Stack>
       </Box>
-      <Box
-        sx={{
-          mt: 20
-        }}
-      >
-        <MediaGrid medias={medias} mediaType={mediaType} />
-        {
-          medias.length !== 0 && <LoadingButton
-            sx={{ marginTop: 10 }}
-            fullWidth
-            color="primary"
-            loading={mediaLoading}
-            onClick={onLoadMore}
-          >
-            load more
-          </LoadingButton>
-        }
+      <Box sx={{ ...uiConfigs.style.mainContent, mb: 2 }}>
+        <Box
+          sx={{
+            mt: 20
+          }}
+        >
+          <MediaGrid medias={medias} mediaType={mediaType} />
+          {
+            medias.length !== 0 && <LoadingButton
+              sx={{ mt: { xs: 2, lg: 3 } }}
+              fullWidth
+              color="primary"
+              loading={mediaLoading}
+              onClick={onLoadMore}
+            >
+              load more
+            </LoadingButton>
+          }
+        </Box>
       </Box>
-    </Box>
-  );
-};
+    </>
+  )
+}
 
-export default GenereList;
+export default GenereList
