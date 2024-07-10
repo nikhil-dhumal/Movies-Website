b30cb5f404593f27f61ddcddd1c4b2942d6fa903
Nikhil241103 <pekacoclover@gmail.com>
2024-07-10 19:45:19 +0530

    rearranged imports

--- client/src/components/common/BackdropSlide.jsx
+++ client/src/components/common/BackdropSlide.jsx
@@ -9,16 +9,18 @@ import tmdbConfigs from "../../api/configs/tmdb.configs"
 const BackdropSlide = ({ backdrops }) => {
   return (
     <NavigationSwiper>
-      {[...backdrops].splice(0, 10)?.map((item, index) => (
-        <SwiperSlide key={index}>
-          <Box sx={{
-            paddingTop: "60%",
-            backgroundPosition: "top",
-            backgroundSize: "cover",
-            backgroundImage: `url(${tmdbConfigs.backdropPath(item.file_path)})`
-          }} />
-        </SwiperSlide>
-      ))}
+      {
+        [...backdrops].splice(0, 10)?.map((item, index) => (
+          <SwiperSlide key={index}>
+            <Box sx={{
+              paddingTop: "60%",
+              backgroundPosition: "top",
+              backgroundSize: "cover",
+              backgroundImage: `url(${tmdbConfigs.backdropPath(item.file_path)})`
+            }} />
+          </SwiperSlide>
+        ))
+      }
     </NavigationSwiper>
   )
 }
