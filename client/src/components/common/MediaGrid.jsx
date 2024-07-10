782ce8a530e98b8afc8582a7bedd928904488c88
Nikhil241103 <pekacoclover@gmail.com>
2024-07-10 16:08:35 +0530

    made content more readable

--- client/src/components/common/MediaGrid.jsx
+++ client/src/components/common/MediaGrid.jsx
@@ -5,11 +5,13 @@ import MediaItem from "./MediaItem"
 const MediaGrid = ({ medias, mediaType }) => {
   return (
     <Grid container spacing={1} sx={{ mx: "-8px!important" }}>
-      {medias?.map((media, index) => (
-        <Grid item xs={6} sm={4} md={3} key={index}>
-          <MediaItem media={media} mediaType={mediaType} />
-        </Grid>
-      ))}
+      {
+        medias?.map((media, index) => (
+          <Grid item xs={6} sm={4} md={3} key={index}>
+            <MediaItem media={media} mediaType={mediaType} />
+          </Grid>
+        ))
+      }
     </Grid>
   )
 }
