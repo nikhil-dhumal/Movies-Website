bd3ef3b4452e160381acf42d46958d7fac8b6121
Nikhil241103 <pekacoclover@gmail.com>
2024-07-10 15:22:42 +0530

    added param sort_by

--- client/src/api/modules/genre.api.js
+++ client/src/api/modules/genre.api.js
@@ -2,7 +2,7 @@ import publicClient from "../client/public.client"
 
 const genreEndpoints = {
   list: '/genres/list',
-  mediasList: ({ mediaType, genre, page }) => `/genres/${mediaType}?with_genres=${genre}&page=${page}`
+  mediasList: ({ mediaType, genre, sort_by, page }) => `/genres/${mediaType}?with_genres=${genre}&sort_by=${sort_by}&page=${page}`
 }
 
 const genreApi = {
@@ -13,9 +13,9 @@ const genreApi = {
       return { response }
     } catch (err) { return { err } }
   },
-  getMediasList: async ({ mediaType, genre, page }) => {
+  getMediasList: async ({ mediaType, genre, sort_by, page }) => {
     try {
-      const response = await publicClient.get(genreEndpoints.mediasList({ mediaType, genre, page }))
+      const response = await publicClient.get(genreEndpoints.mediasList({ mediaType, genre, sort_by, page }))
 
       return { response }
     } catch (err) { return { err } }
