8d6ee4072eea1b3d0d96a85119949af2cb8f0fb7
Nikhil241103 <pekacoclover@gmail.com>
2024-07-21 22:49:01 +0530

    added styles for scroll bar

--- client/src/index.jsx
+++ client/src/index.jsx
@@ -11,6 +11,8 @@ import "@fontsource/roboto/400.css"
 import "@fontsource/roboto/500.css"
 import "@fontsource/roboto/700.css"
 
+import "./styles.css"
+
 const root = ReactDOM.createRoot(document.getElementById("root"))
 root.render(
   // <React.StrictMode>
